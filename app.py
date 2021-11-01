9028e12bd56b38c65348381288b6ea1c71cbf96f
popam <popa.mariusz@gmail.com>
2021-11-01 20:44:40 +0100

    doinstalowanie sqlalchemy

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request
 from accountant import manager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
 
 
 @app.route("/", methods=["GET", "POST"])
